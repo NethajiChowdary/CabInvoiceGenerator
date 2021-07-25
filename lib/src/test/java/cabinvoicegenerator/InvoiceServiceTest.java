9137ef4da9d3c8ec8074b86353561c437e1abf79
NethajiChowdary <nethajichowdary555@gmail.com>
2021-07-25 23:22:32 +0530

    Added: Code to Calculate The Aggregate Total For Multiple Rides

--- lib/src/test/java/cabinvoicegenerator/InvoiceServiceTest.java
+++ lib/src/test/java/cabinvoicegenerator/InvoiceServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 public class InvoiceServiceTest
 {
+
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare()
     {
@@ -22,7 +23,17 @@ public class InvoiceServiceTest
         double distance=0.1;
         int time=1;
         double fare=generator.calculateFare(distance,time);
-        Assert.assertEquals(5.0,fare,0.0);
+        Assert.assertEquals(2.0,fare,0.0);
     }
 
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare()
+    {
+        InvoiceGenerator generator=new InvoiceGenerator();
+        Ride[] rides={  new Ride(2.0,5),
+                        new Ride(0.1,1)
+                    };
+        double fare=generator.calculateFare(rides);
+        Assert.assertEquals(27,fare,0.0);
+    }
 }
