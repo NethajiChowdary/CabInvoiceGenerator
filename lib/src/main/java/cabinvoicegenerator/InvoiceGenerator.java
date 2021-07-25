f35d3576750a2f8e0b5766ed7564799e086f474d
NethajiChowdary <nethajichowdary555@gmail.com>
2021-07-25 23:32:47 +0530

    Added: Code to Return Total Number of Rides, Total Fare, Average Fare Per Ride

--- lib/src/main/java/cabinvoicegenerator/InvoiceGenerator.java
+++ lib/src/main/java/cabinvoicegenerator/InvoiceGenerator.java
@@ -1,6 +1,6 @@
 package cabinvoicegenerator;
 
-public class InvoiceGenerator 
+public class InvoiceGenerator
 {
 
     public static void main(String[] args) 
@@ -10,19 +10,21 @@ public class InvoiceGenerator
 
     private static final int COST_PER_TIME=1;
     private static final double MINIMUM_COST_PER_KILOMETER=10;
+    private static final double MINIMUM_FARE=5;
 
-    public double calculateFare(double distance,int time)
+    public double calculateFare(double distance,int time) 
     {
-        return distance*MINIMUM_COST_PER_KILOMETER+time*COST_PER_TIME;
+        double totalFare= distance*MINIMUM_COST_PER_KILOMETER+time*COST_PER_TIME;
+        return Math.max(totalFare, MINIMUM_FARE);
     }
 
-    public double calculateFare(Ride[] rides) 
+    public InvoiceSummary calculateFare(Ride[] rides) 
     {
-        double totalFare=0;
+        double totalFare = 0;
         for(Ride ride:rides) 
         {
-            totalFare+=this.calculateFare(ride.distance, ride.time);
+            totalFare += this.calculateFare(ride.distance, ride.time);
         }
-        return totalFare;
+        return new InvoiceSummary(rides.length,totalFare);
     }
 }
